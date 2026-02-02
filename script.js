7c6bc678d84dead34ec12abfae8f6e25c4734f15
Yashika Khurana <Yashikakhurana17@gmail.com>
2026-02-02 21:55:05 +0530

    console API

---
+++ script.js
@@ -0,0 +1,7 @@
+//console.log(`Hello`);
+//window.alert(`This is alert message`);
+//alert("Javascript connected");
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I am yashika.`;
+//let x=113;
+//console.log(x);
