2deb32b1cf649c85c60fdba742ba90e0d1692ec0
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-02-04 03:02:41 +0530

    Operators

--- script.js
+++ script.js
@@ -11,28 +11,62 @@
 // console.log(number1 + number2);
 //DATA TYPES
 //NUMBERS
-var num = 99.2;
-//STRING
-var str1 = "This is string";
-var str2 = 'This is also string';
-//OBJECTS
-var marks = {
-    Ayush: 33,
-Yashika: 45,
-}
-console.log(num);
-console.log(marks);
-//BOOLEAN
-var a = true;
-var b = false;
-console.log(a);
-//UNDEFINED
-var und;
-console.log(und);
-//NULL
-var n = null;
-console.log(n);
+// var num = 99.2;
+// //STRING
+// var str1 = "This is string";
+// var str2 = 'This is also string';
+// //OBJECTS
+// var marks = {
+//     Ayush: 33,
+// Yashika: 45,
+// }
+// console.log(num);
+// console.log(marks);
+// //BOOLEAN
+// var a = true;
+// var b = false;
+// console.log(a);
+// //UNDEFINED
+// var und;
+// console.log(und);
+// //NULL
+// var n = null;
+// console.log(n);
 //ARRAYS
-var arr = [1,4,"Java script", 5,2];
-console.log(arr);
-console.log(arr[2]);
+//  var arr = [1,4,"Java script", 5,2];
+// console.log(arr);
+// console.log(arr[2]);
+//OPERATORS
+//ARETHMETIC OP
+// var a = 88;
+// var b = 44;
+// console.log("The value of a + b is: ", a+b);
+// console.log("The value of a - b is: ", a-b);
+// console.log("The value of a * b is: ", a*b);
+// console.log("The value of a / b is: ", a/b);
+//ASSIGNMENT OP
+// var c = b;
+// c +=2;
+// c-=3;
+// c*=2;
+// c/=2;
+// console.log(c);
+//COMPARISON OP
+var x = 66;
+var y = 33;
+//console.log(x == y);
+// console.log(x>=y);
+// console.log(x<=y);
+// console.log(x>y);
+// console.log(x<y);
+//LOGICAL OP
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+
+console.log(!true);
+console.log(!false);
