3f94dcebb1994ad285bb819ecfd61697ae895ae7
Yashika Khurana <yashikaakhurana17@gmail.com>
2026-02-04 02:40:34 +0530

    Data types

--- script.js
+++ script.js
@@ -1,7 +1,38 @@
 //console.log(`Hello`);
 //window.alert(`This is alert message`);
 //alert("Javascript connected");
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I am yashika.`;
+// document.getElementById("myH1").textContent = `Hello`;
+// document.getElementById("myP").textContent = `I am yashika.`;
 //let x=113;
 //console.log(x);
+//JAVASCRIPT VARIABLES
+// var number1 = 99;
+// var number2 = 43;
+// console.log(number1 + number2);
+//DATA TYPES
+//NUMBERS
+var num = 99.2;
+//STRING
+var str1 = "This is string";
+var str2 = 'This is also string';
+//OBJECTS
+var marks = {
+    Ayush: 33,
+Yashika: 45,
+}
+console.log(num);
+console.log(marks);
+//BOOLEAN
+var a = true;
+var b = false;
+console.log(a);
+//UNDEFINED
+var und;
+console.log(und);
+//NULL
+var n = null;
+console.log(n);
+//ARRAYS
+var arr = [1,4,"Java script", 5,2];
+console.log(arr);
+console.log(arr[2]);
